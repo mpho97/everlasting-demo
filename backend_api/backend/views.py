d47bac1ba0c4667de7c2ff67648f84527cbaf49f
Mpho Johannes Tema <mphotema97@gmail.com>
2025-10-21 23:38:58 -0700

    Delete backend_api directory

--- backend_api/backend/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.urls import path
-from django.http import HttpResponse
-
-# Create your views here.
-def Contact(request):
-    return HttpResponse("hello world")
